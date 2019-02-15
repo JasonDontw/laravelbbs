a626393d90453406277aab084a5af5a4b5ea6c3b
Jason Dong <ee13751@gmail.com>
2019-02-15 09:31:53 +0800

    刪除評論

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -53,7 +53,13 @@ class Topic extends Model
     }
 
     public function link($params = [])
-{
+    {
     return route('topics.show', array_merge([$this->id, $this->slug], $params));
-}
+    }
+
+    public function updateReplyCount()
+    {
+    $this->reply_count = $this->replies->count();
+    $this->save();
+    }
 }
